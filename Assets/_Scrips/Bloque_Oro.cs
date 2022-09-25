91bc2c51e9302bdf3e5637bc0cc0fecbbb097ce5
OvertNebula <96168378+OvertNebula@users.noreply.github.com>
2022-09-25 04:06:05 -0500

    Practica10-Vercion1
    -primera practica del Modulo10

--- Assets/_Scrips/Bloque_Oro.cs
+++ Assets/_Scrips/Bloque_Oro.cs
@@ -2,18 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bloque_Piedra : Bloque
+public class Bloque_Oro : Bloque
 {
     // Start is called before the first frame update
     void Start()
     {
-        resistencia = 5;
+        resistencia = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+
     }
 
     public override void RebotarBola()
--- Assets/_Scrips/Bloque_Tierra.cs
+++ Assets/_Scrips/Bloque_Tierra.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bloque_Piedra : Bloque
+public class Bloque_Tierra : Bloque
 {
     // Start is called before the first frame update
     void Start()
     {
-        resistencia = 5;
+        resistencia = 1;
     }
 
     // Update is called once per frame
@@ -15,7 +15,6 @@ public class Bloque_Piedra : Bloque
     {
         
     }
-
     public override void RebotarBola()
     {
         base.RebotarBola();
