31b7baff065a1a4d13bd402ed65ea18604dcfc98
OvertNebula <96168378+OvertNebula@users.noreply.github.com>
2023-01-24 00:34:36 -0600

    practia12
    inicio de la practica 12

--- Assets/_Scrips/JugadorPleyer.cs
+++ Assets/_Scrips/JugadorPleyer.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class JugadorPleyer : MonoBehaviour
 {
+    [SerializeField] public int limiteX = 23;
+    [SerializeField] public float velocidadPaddle = 5f; //velocidad de movimiento 
+
+    Transform transform;
+    Vector3 mousePos2D;
+    Vector3 mousePos3D;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform = this.gameObject.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+
+
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    //mueve el objeto derecha
+        //    transform.Translate(Vector3.down * velocidadPaddle * Time.deltaTime);
+        //}
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        //{t
+        //    ransform.Translate(Vector3.up * velocidadPaddle * Time.deltaTime);
+        //    //mueve el objeto a la izquierda
+        //}
+
+        transform.Translate(Input.GetAxis("Horizontal") * Vector3.down * velocidadPaddle * Time.deltaTime);
+
+        //Vector3 pos = transform.position;
+        //pos.x = mousePos3D.x;
+        if (pos.x < -limiteX)
+        {
+            pos.x = -limiteX;
+        }
+        else if(pos.x > limiteX)
+        {
+            pos.x = limiteX;
+        }
+        this.transform.position = pos;
     }
 }
