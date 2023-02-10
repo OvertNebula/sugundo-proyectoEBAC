97840537d4075a8550932f801ccfc35678636fd5
OvertNebula <96168378+OvertNebula@users.noreply.github.com>
2023-02-10 04:03:23 -0600

    prectica-13.v1
    -error en Unity

---
+++ Assets/_scrptable objets/PuntajeMaximo.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PuntajeMaximo", menuName = "Tools/PuntajeMaximo", order =0)]
+public class puntajeMaximo : ScriptableObject
+{
+    public int Puntaje = 0;
+    public int PuntajeMaximo = 10000;
+}
